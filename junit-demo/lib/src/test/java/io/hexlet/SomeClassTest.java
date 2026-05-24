e0a127d848c361feb0a4bb50b7aa6736ec289779
Pavel <bedrevpaul23@gmail.com>
2026-05-25 01:09:41 +0300

    Add JUnit demo project

---
+++ junit-demo/lib/src/test/java/io/hexlet/SomeClassTest.java
@@ -0,0 +1,16 @@
+package io.hexlet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SomeClassTest {
+
+    @Test
+    public void testSum() {
+        var expected = 5;
+        var actual = SomeClass.sum(3, 2);
+
+        assertEquals(expected, actual);
+    }
+}
